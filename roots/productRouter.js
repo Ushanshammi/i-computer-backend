bb990a90ea391dbcf00c3cd59f4e1bbe2d14ab12
UshanShammi <ushanshammika0@gmail.com>
2025-10-12 09:45:17 +0530

    add-comment

--- roots/productRouter.js
+++ roots/productRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import { createProduct, deleteProduct, getAllProducts, getProductByID, updateProduct } from "../controller/productController.js";
 
 const productRouter=express.Router();
-
+//routers
 productRouter.get("/",getAllProducts);
 productRouter.post("/",createProduct);
 productRouter.put("/:productID",updateProduct);
